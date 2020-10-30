1aff71959d525f0e0d3efbaeb513c95d21e5917e
Juan Mariñez <juan.dsmarinez@gmail.com>
2020-10-29 23:51:52 -0400

    [ADD] Stream controller for popular movies

--- lib/src/providers/movie_provider.dart
+++ lib/src/providers/movie_provider.dart
@@ -1,5 +1,5 @@
+import 'dart:async';
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 import 'package:moviesapp/src/models/movie.dart';
@@ -8,6 +8,20 @@ class MovieProvider {
   String _apiKey = 'ad41c18aa85afbcd874a406cdfd08fb6';
   String _url = 'api.themoviedb.org';
   String _language = 'en-US';
+
+  int _moviePages = 0;
+
+  List<Movie> _movies = List<Movie>();
+
+  final _popularMovieStreamController = StreamController();
+
+  Function(List<Movie>) get _popularMoviesSink => _popularMovieStreamController.sink.add;
+
+  Stream<List<Movie>> get _popularMoviesStream => _popularMovieStreamController.stream;
+
+  void dispose() {
+    _popularMovieStreamController?.close();
+  }
 
   Future<List<Movie>> getNowPlaying() async {
     final url = Uri.https(
@@ -28,6 +42,9 @@ class MovieProvider {
   }
 
   Future<List<Movie>> getPopularMovies() async {
+
+    _moviePages++;
+
     final url = Uri.http(
       _url,
       '3/movie/popular',
@@ -41,6 +58,9 @@ class MovieProvider {
     final decodedData = json.decode(response.body);
 
     final movies = Movies.fromJsonList(decodedData['results']);
+
+    _movies.addAll(movies.movies);
+    _popularMoviesSink(movies.movies);
 
     return movies.movies;
   }
